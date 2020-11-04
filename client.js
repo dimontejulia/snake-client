423611e6384518eee5ad01e58358160e8a56f5ec
Julia Di Monte <dimontejulia@gmail.com>
2020-11-04 22:15:47 +0000

    adding constants for hard coded values

--- client.js
+++ client.js
@@ -2,12 +2,11 @@
  * Establishes connection with the game server
  */
 const net = require("net");
+const { IP, PORT } = require("./constants");
 const connect = function () {
   const conn = net.createConnection({
-    // host: "135.23.222.131",
-    // port: 50542,
-    host: "localhost",
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
---
+++ constants.js
@@ -0,0 +1,13 @@
+const IP = "localhost";
+const PORT = 50541;
+
+/*
+Class Zoom Values:
+host: "135.23.222.131",
+port: 50542,
+*/
+
+module.exports = {
+  IP,
+  PORT,
+};

8d7d0755da974e229e869cef5bf7e86be1793728
Julia Di Monte <dimontejulia@gmail.com>
2020-11-04 21:05:34 +0000

    snake moves with wasd keys

--- input.js
+++ input.js
@@ -1,19 +1,29 @@
+let connection;
+
 /* Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => handleUserInput(key));
   return stdin;
 };
 
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 };
 

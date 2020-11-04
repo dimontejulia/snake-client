3ba3db74ba8df95fa0fd0610c68faa865c5dcbd7
Julia Di Monte <dimontejulia@gmail.com>
2020-11-04 19:10:50 +0000

    added our name and a connection message

--- client.js
+++ client.js
@@ -4,11 +4,18 @@
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
+    host: "135.23.222.131",
+    port: 50542,
+    // host: "localhost",
+    // port: 50541,
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on(
+    "connect",
+    () => console.log("Successfully connected to game server"),
+    conn.write("Name: JD")
+  );
   console.log("you ded cuz you idled");
   return conn;
 };
